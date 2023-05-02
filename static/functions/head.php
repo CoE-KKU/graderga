84e01c064b59c8025e88bee2e98fa9770653ee83
PondJaᵀᴴ <18050176+p0ndja@users.noreply.github.com>
2023-05-03 00:07:34 +0700

    Added GA tag

--- static/functions/head.php
+++ static/functions/head.php
@@ -68,3 +68,12 @@
     <script src='https://www.hCaptcha.com/1/api.js' async defer></script>
 
     <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5249401084148971" crossorigin="anonymous"></script>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-WD5R13YM08"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-WD5R13YM08');
+    </script>
