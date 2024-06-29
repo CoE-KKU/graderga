8798f8be126b837decc2d7c18c3c30e86626516a
p0ndja <palapon2545@gmail.com>
2024-06-29 14:51:23 +0700

    Update submission_gen.php

--- pages/submission_gen.php
+++ pages/submission_gen.php
@@ -17,31 +17,33 @@
                 $subRuntime = $row['runningtime']; //ms
                 $subMemory = $row['memory'] ? $row['memory'] . " MB": randomErrorMessage(); //MB
                 $subUploadtime = $row['uploadtime']; ?>
-                <p>User: <code><?php echo $subUser; ?></code>
+                <p>Timestamp: <?php echo $subUploadtime; ?>
+                <br>User: <code><?php echo $subUser; ?></code>
                 <br>Problem: <a href="../problem/<?php echo $subProb; ?>"><?php $spb = new Problem($subProb); echo $spb->display(); ?></a>
                 <br>Language: <code><?php echo $subLang; ?></code>
                 <br>Result: <code <?php if ($row['result'] == 'W') echo "data-sub-id='$id' data-wait=true"; ?>><?php echo $subResult; ?></code>
                 <br>Running Time: <code><?php echo $subRuntime; ?> ms</code>
                 <!-- <br>Memory: <code><?php //echo $subMemory; ?></code> -->
-                <br>Submit Time: <?php echo $subUploadtime; ?>
+                
                 </p>
                 <?php if ($row['user'] == $wholookthis || isAdmin()) {
-                    echo "<pre><code>";
+                    if ($row['comment'] != "End of Test") {
+                        echo "Judge Response:<br><pre>" . $row['comment'] . "</pre>";
+                    }
+                    echo "Submitted Code: ";
+                    if (file_exists($row['script'])) { ?>
+                        <p class="text-right mb-0 mt-0"><small><a href="<?php echo $row['script']; ?>" download="<?php echo $subID.".".pathinfo($row['script'], PATHINFO_EXTENSION);?>" target="_blank">Download Code <i class="fas fa-download"></i></a></small></p>
+                    <?php }
                     if (file_exists($row['script'])) {
+                        echo "<br><pre><code>";
                         $r = file_get_contents($row['script']);
                         $r = str_replace("<", "&lt;", $r); //Make browser don't think < is the start of html tag
                         $r = str_replace(">", "&gt;", $r); //Make browser don't think < is the end of html tag
                         echo ($r);
-                        if ($row['comment'] != "End of Test") {
-                            echo "\n\n\n//--------------------[JUDGE RESPONSE]--------------------\n" . $row['comment'] . "\n//--------------------[JUDGE RESPONSE]--------------------";
-                        }
+                        echo "</code></pre>";
                     } else {
-                        echo "FILE NOT FOUND.";
-                    }
-                    echo "</code></pre>"; ?>
-                    <?php if (file_exists($row['script'])) { ?>
-                        <p class="text-right mb-0 mt-0"><small><a href="<?php echo $row['script']; ?>" download="<?php echo $subID.".".pathinfo($row['script'], PATHINFO_EXTENSION);?>" target="_blank">Download Code <i class="fas fa-download"></i></a></small></p>
-                    <?php } ?>
+                        echo "<span class='text-danger'>MISSING FILE</span>";
+                    } ?>
                     <?php 
                 } ?>
             <?php }
