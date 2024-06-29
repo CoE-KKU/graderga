1e79ad7ea3a4d7d240c5a7905c6742527b798755
p0ndja <palapon2545@gmail.com>
2024-06-29 15:16:39 +0700

    *

--- static/functions/popup.php
+++ static/functions/popup.php
@@ -10,7 +10,7 @@
                 </button>
             </div>
             <div class="modal-body">
-                <img src="https://s3.p0nd.dev/file/d52fa/banner.jpg"
+                <img src="https://i.imgur.com/Vrj8O8s.jpg"
                     class="img-fluid w-100 d-flex justify-content-center mb-3 z-depth-2">
                 <div class="modal-text">
                     <p>ทางผู้พัฒนาขอความร่วมมือจากผู้เข้าชมเว็บไซต์ทุก ๆ ท่าน ร่วมตอบแบบสำรวจความพึงพอใจในการใช้งานเว็บไซต์ <a href="https://grader.pondja.com/">grader.pondja.com</a> และ/หรือ <a href="https://lca.pondja.com/">lca.pondja.com</a><br><p class="text-right">ขอบคุณครับ<br>PondJa<sup>TH</sup></p>
