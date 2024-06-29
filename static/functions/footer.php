a2f5645fe07837dc03fc3757678696546daa0009
p0ndja <palapon2545@gmail.com>
2024-06-29 14:42:52 +0700

    Update footer.php

--- static/functions/footer.php
+++ static/functions/footer.php
@@ -1,9 +1,16 @@
+<?php
+    function randomFooterEmoji() {
+        $emoji = array("☕️","🧠","💻","🐾","❤️","🙌","💖","💝","🫶","😴");
+        return $emoji[array_rand($emoji)];
+    }
+?>
 <footer id="footer" class="footer">
     <div class="container-fluid">
         <div class="text-center mb-3">
             <hr style="opacity: 0.3;">
-            <b>Grader.ga</b> - Made with <b style="color: salmon; ">Meow🐾</b> by <a href="https://github.com/p0ndja/" class="font-weight-bold" target="_blank">p0ndja</a> & <a href="https://github.com/Nepumi-Jr/" class="font-weight-bold" target="_blank">Nepumi</a>
-            <br><sup><small class="text-muted" style="opacity:0.75;"><?php $target = "../version.txt"; if (file_exists($target)) echo "Version " . fread(fopen($target, "r"),filesize($target)); ?> • <?php $end_time = microtime(TRUE); $time_taken =($end_time - $start_time)*1000; $time_taken = round($time_taken,2); echo 'Page generated in ' . $time_taken . ' ms.';?> • Visual from <a href="https://www.flaticon.com/" title="Flaticon" target="_blank">Flaticon</a>, <a href="https://www.freepik.com/" title="Freepik" target="_blank">Freepik</a> and <a href="https://fontawesome.com/" title="Fontawesome" target="_blank">Fontawesome</a></small></sup>
+            <b>Garedami</b> - Made with <?php echo randomFooterEmoji(); ?> by <a href="https://github.com/p0ndja/" class="font-weight-bold" target="_blank">p0ndja</a> & <a href="https://github.com/Nepumi-Jr/" class="font-weight-bold" target="_blank">Nepumi</a><br>
+            <span class="text-muted">If you have any question or suggestion, Feel free to contact admin via email <a href="mailto:palapon@kkumail.com">palapon@kkumail.com</a></span><br>
+            <sup><small class="text-muted" style="opacity:0.75;"><?php $target = "../version.txt"; if (file_exists($target)) echo "Version " . fread(fopen($target, "r"),filesize($target)); ?> • <?php $end_time = microtime(TRUE); $time_taken =($end_time - $start_time)*1000; $time_taken = round($time_taken,2); echo 'Page generated in ' . $time_taken . ' ms.';?> • Visual from <a href="https://www.flaticon.com/" title="Flaticon" target="_blank">Flaticon</a>, <a href="https://www.freepik.com/" title="Freepik" target="_blank">Freepik</a> and <a href="https://fontawesome.com/" title="Fontawesome" target="_blank">Fontawesome</a></small></sup>
         </div>
     </div>
 </footer>
